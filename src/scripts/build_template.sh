1dfdc5f8eb6b57f03860be52766bdb2f5e183ab7
Giovanni Silva <giovanni@kugelbit.com>
2023-02-19 03:17:02 -0300

    lint

--- src/scripts/build_template.sh
+++ src/scripts/build_template.sh
@@ -2,15 +2,15 @@
 
 SHORT_SHA1=$(echo -n "$CIRCLE_SHA1" | head -c 7)
 if [ "$(echo $CIRCLE_REPOSITORY_URL | grep ^git@github.com)" ]; then
-    COMMIT_LINK=\[$SHORT_SHA1\]\(https://github.com/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/commit/$CIRCLE_SHA1\)
+    COMMIT_LINK=[$SHORT_SHA1](https://github.com/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/commit/$CIRCLE_SHA1)
 elif [ "$(echo $CIRCLE_REPOSITORY_URL | grep ^git@bitbucket.org)" ]; then
-    COMMIT_LINK=\[$SHORT_SHA1\]\(https://bitbucket.org/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/commits/$CIRCLE_SHA1\)
+    COMMIT_LINK=[$SHORT_SHA1](https://bitbucket.org/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/commits/$CIRCLE_SHA1)
 else
     >&2 echo unknown version control system: "$CIRCLE_REPOSITORY_URL"
     fail
 fi
 
-KEYBASE_MESSAGE_TEMPLATE=$(cat \<<<END_HEREDOC
+KEYBASE_MESSAGE_TEMPLATE=$(cat \<<END_HEREDOC
     CircleCI Build Notification
     "## __build_status__ "$CIRCLE_PROJECT_REPONAME" [#${CIRCLE_BUILD_NUM}]\("$CIRCLE_BUILD_URL"\)"
     Job: [${CIRCLE_JOB}]\("$CIRCLE_BUILD_URL"\)
