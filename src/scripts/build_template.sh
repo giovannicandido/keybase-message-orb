b549413801ce204f8f2f2a7cb70248bdb319116b
Giovanni Silva <giovanni@kugelbit.com>
2023-02-19 02:59:09 -0300

    lint

--- src/scripts/build_template.sh
+++ src/scripts/build_template.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
 SHORT_SHA1=$(echo -n "$CIRCLE_SHA1" | head -c 7)
-if [ "$(echo \"$CIRCLE_REPOSITORY_URL\" | grep -q '^git@github.com')" ]; then
+if [ "$(echo $CIRCLE_REPOSITORY_URL | grep ^git@github.com)" ]; then
     COMMIT_LINK=\[$SHORT_SHA1\]\(https://github.com/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/commit/$CIRCLE_SHA1\)
-elif [ "$(echo \"$CIRCLE_REPOSITORY_URL\" | grep -q '^git@bitbucket.org')" ]; then
+elif [ "$(echo $CIRCLE_REPOSITORY_URL | grep ^git@bitbucket.org)" ]; then
     COMMIT_LINK=\[$SHORT_SHA1\]\(https://bitbucket.org/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/commits/$CIRCLE_SHA1\)
 else
     >&2 echo unknown version control system: "$CIRCLE_REPOSITORY_URL"
@@ -10,7 +11,7 @@ else
 fi
 # Note that the "\<<" in the heredoc declaration is escaped from
 # CircleCI's parameters syntax.
-KEYBASE_MESSAGE_TEMPLATE=$(cat \<<END_HEREDOC
+KEYBASE_MESSAGE_TEMPLATE=$(cat <<END_HEREDOC
     CircleCI Build Notification
     "## __build_status__ "$CIRCLE_PROJECT_REPONAME" [#${CIRCLE_BUILD_NUM}]\("$CIRCLE_BUILD_URL"\)"
     Job: [${CIRCLE_JOB}]\("$CIRCLE_BUILD_URL"\)
