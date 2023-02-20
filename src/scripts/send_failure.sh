c1c38c4a3e19fa7bfc696ead5e87cef097890c16
Giovanni Silva <giovanni@kugelbit.com>
2023-02-19 22:21:10 -0300

    test

--- src/scripts/send_failure.sh
+++ src/scripts/send_failure.sh
@@ -1,3 +1,5 @@
-sed -e 's/__build_status__/FAILED/' -e 's/__theme_color__/14a603/' .keybase_message > .keybase_message
+sed -i 's/__build_status__/FAILED/' -i 's/__theme_color__/14a603/' .keybase_message
 export MESSAGE=`cat .keybase_message`
+echo "sending message: "
+echo ${MESSAGE}
 node /app/out/main.js
